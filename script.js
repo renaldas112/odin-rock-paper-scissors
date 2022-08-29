9367048ab6b2e54222da3700b6f409d1106aea34
renaldas112 <renaldas112@gmail.com>
2022-08-29 20:20:30 +0300

    created design using bootstrap

---
+++ script.js
@@ -0,0 +1,11 @@
+//submit button doesn't refresh the page
+
+// let BUTTON = document.querySelector("button");
+// input.addEventListener("submit", (event) => {
+//   event.preventDefault();
+//   console.log("veikia?");
+// })
+
+const veikia = "veikia?";
+
+console.log(veikia);
