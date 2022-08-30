1d5417ec8d98c9b3babb4d8fb12724bfbb266246
renaldas112 <renaldas112@gmail.com>
2022-08-30 17:15:16 +0300

    added computer random selection

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
-function playRound(playerSelection, computerSelection) {
-    // your code here!
-  }
-   
-  const playerSelection = "rock";
-  const computerSelection = getComputerChoice();
-  console.log(playRound(playerSelection, computerSelection));
+const rock = document.querySelector(".rock");
+const paper = document.querySelector(".paper");
+const scissors = document.querySelector(".scissors");
 
-let BUTTON = document.querySelectorAll(".button-1, .button-2, .button-3");
+// computer choice
+const getComputerChoice = () => {
+  const computerChoice = ["Rock", "Paper", "Scissors"];
 
-[].forEach.call(BUTTON, function (btn) {
-  btn.addEventListener("click", function (event) {
-    console.log("veikia?");
-  });
-});
+  const random = Math.floor(Math.random() * computerChoice.length);
+  console.log(random, computerChoice[random]);
+};
 
+const computerSelection = getComputerChoice();
+
+
+
+// function playRound(playerSelection, computerSelection) {
+//   // your code here!
+// }
+// console.log(playRound(playerSelection, computerSelection));
